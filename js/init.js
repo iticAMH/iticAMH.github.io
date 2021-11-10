d7e99a5b7af36d7a1c9d72bc8fa0cfde93f53a1d
iticAMH <94078410+iticAMH@users.noreply.github.com>
2021-11-10 14:15:40 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDDQ3rLAsioeRXuwW_V5p4H5GflUo42vPQ",
+  authDomain: "manejonum.firebaseapp.com",
+  projectId: "manejonum",
+  storageBucket: "manejonum.appspot.com",
+  messagingSenderId: "696505080396",
+  appId: "1:696505080396:web:35bf5b3d2a638614edb2e9",
+  measurementId: "G-001RL462G4"
+    });
